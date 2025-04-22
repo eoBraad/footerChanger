aaa713d54a74efdcc74f8c7e1002976a9789112e
João Vitor Alencar Pontes <joao.pontes@software.com.br>
2025-04-22 17:43:57 -0300

    feat: add a edit doc method and spire.doc in project

--- FooterChanger/HomeScreen.Designer.cs
+++ FooterChanger/HomeScreen.Designer.cs
@@ -31,196 +31,225 @@ partial class HomeScreen
     /// </summary>
     private void InitializeComponent()
     {
-        System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(HomeScreen));
-        _treeView = new System.Windows.Forms.TreeView();
-        changeFolder = new System.Windows.Forms.Button();
-        comboBox1 = new System.Windows.Forms.ComboBox();
-        Impressora = new System.Windows.Forms.Label();
-        button1 = new System.Windows.Forms.Button();
-        label1 = new System.Windows.Forms.Label();
-        textBox1 = new System.Windows.Forms.TextBox();
-        label2 = new System.Windows.Forms.Label();
-        textBox2 = new System.Windows.Forms.TextBox();
-        textBox3 = new System.Windows.Forms.TextBox();
-        label3 = new System.Windows.Forms.Label();
-        button2 = new System.Windows.Forms.Button();
-        label4 = new System.Windows.Forms.Label();
-        comboBox2 = new System.Windows.Forms.ComboBox();
+        _treeView = new TreeView();
+        changeFolder = new Button();
+        printInput = new ComboBox();
+        Impressora = new Label();
+        label1 = new Label();
+        ficInput = new TextBox();
+        label2 = new Label();
+        pagina1Input = new TextBox();
+        pagina2Input = new TextBox();
+        label3 = new Label();
+        SaveAndPrint = new Button();
+        saveButton = new Button();
+        selectedItem = new Label();
+        selectedFilePath = new Label();
         SuspendLayout();
         // 
         // _treeView
         // 
-        _treeView.BackColor = System.Drawing.Color.FromArgb(((int)((byte)177)), ((int)((byte)161)), ((int)((byte)204)));
-        _treeView.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)0));
-        _treeView.Location = new System.Drawing.Point(14, 70);
+        _treeView.BackColor = Color.FromArgb(177, 161, 204);
+        _treeView.Font = new Font("Segoe UI", 10.2F, FontStyle.Regular, GraphicsUnit.Point, 0);
+        _treeView.Location = new Point(11, 61);
+        _treeView.Margin = new Padding(2);
         _treeView.Name = "_treeView";
-        _treeView.Size = new System.Drawing.Size(264, 599);
+        _treeView.Size = new Size(355, 633);
         _treeView.TabIndex = 0;
         _treeView.AfterSelect += _treeView_AfterSelect;
-        _treeView.AddBorderRadious(20);
         // 
         // changeFolder
         // 
-        changeFolder.BackColor = System.Drawing.Color.FromArgb(((int)((byte)177)), ((int)((byte)161)), ((int)((byte)204)));
-        changeFolder.Cursor = System.Windows.Forms.Cursors.Hand;
-        changeFolder.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)((byte)177)), ((int)((byte)161)), ((int)((byte)204)));
+        changeFolder.BackColor = Color.FromArgb(177, 161, 204);
+        changeFolder.Cursor = Cursors.Hand;
+        changeFolder.FlatAppearance.BorderColor = Color.FromArgb(177, 161, 204);
         changeFolder.FlatAppearance.BorderSize = 0;
-        changeFolder.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)((byte)177)), ((int)((byte)161)), ((int)((byte)204)));
-        changeFolder.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)((byte)177)), ((int)((byte)161)), ((int)((byte)204)));
-        changeFolder.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-        changeFolder.Font = new System.Drawing.Font("Segoe UI", 7.2000003F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)0));
-        changeFolder.ForeColor = System.Drawing.Color.WhiteSmoke;
-        changeFolder.Location = new System.Drawing.Point(14, 16);
+        changeFolder.FlatAppearance.MouseDownBackColor = Color.FromArgb(177, 161, 204);
+        changeFolder.FlatAppearance.MouseOverBackColor = Color.FromArgb(177, 161, 204);
+        changeFolder.FlatStyle = FlatStyle.Flat;
+        changeFolder.Font = new Font("Segoe UI", 7.8F, FontStyle.Bold, GraphicsUnit.Point, 0);
+        changeFolder.ForeColor = Color.WhiteSmoke;
+        changeFolder.Location = new Point(11, 11);
+        changeFolder.Margin = new Padding(2);
         changeFolder.Name = "changeFolder";
-        changeFolder.Size = new System.Drawing.Size(157, 35);
+        changeFolder.Size = new Size(156, 37);
         changeFolder.TabIndex = 1;
         changeFolder.Text = "Alterar Diretório";
         changeFolder.UseVisualStyleBackColor = false;
-        changeFolder.AddBorderRadious(20);
         changeFolder.Click += changeFolder_Click;
         // 
-        // comboBox1
+        // printInput
         // 
-        comboBox1.FormattingEnabled = true;
-        comboBox1.Location = new System.Drawing.Point(538, 47);
-        comboBox1.Name = "comboBox1";
-        comboBox1.Size = new System.Drawing.Size(378, 23);
-        comboBox1.TabIndex = 2;
+        printInput.Font = new Font("Segoe UI", 10.8F);
+        printInput.FormattingEnabled = true;
+        printInput.Location = new Point(388, 193);
+        printInput.Margin = new Padding(2);
+        printInput.Name = "printInput";
+        printInput.Size = new Size(281, 33);
+        printInput.TabIndex = 2;
         // 
         // Impressora
         // 
-        Impressora.Location = new System.Drawing.Point(538, 24);
+        Impressora.Font = new Font("Segoe UI", 7.8F);
+        Impressora.ForeColor = Color.WhiteSmoke;
+        Impressora.Location = new Point(388, 168);
+        Impressora.Margin = new Padding(2, 0, 2, 0);
         Impressora.Name = "Impressora";
-        Impressora.Size = new System.Drawing.Size(68, 20);
+        Impressora.Size = new Size(76, 23);
         Impressora.TabIndex = 3;
         Impressora.Text = "Impressora\r\n";
-        Impressora.Click += label1_Click;
-        // 
-        // button1
-        // 
-        button1.Location = new System.Drawing.Point(937, 47);
-        button1.Name = "button1";
-        button1.Size = new System.Drawing.Size(144, 26);
-        button1.TabIndex = 4;
-        button1.Text = "Abrir Fila de Impressão";
-        button1.UseVisualStyleBackColor = true;
         // 
         // label1
         // 
-        label1.Location = new System.Drawing.Point(538, 105);
+        label1.Font = new Font("Segoe UI", 9F, FontStyle.Regular, GraphicsUnit.Point, 0);
+        label1.ForeColor = Color.WhiteSmoke;
+        label1.Location = new Point(388, 105);
+        label1.Margin = new Padding(2, 0, 2, 0);
         label1.Name = "label1";
-        label1.Size = new System.Drawing.Size(32, 20);
+        label1.Size = new Size(36, 21);
         label1.TabIndex = 5;
         label1.Text = "FIC\r\n";
         // 
-        // textBox1
+        // ficInput
         // 
-        textBox1.Location = new System.Drawing.Point(617, 102);
-        textBox1.Name = "textBox1";
-        textBox1.Size = new System.Drawing.Size(299, 23);
-        textBox1.TabIndex = 6;
+        ficInput.Font = new Font("Segoe UI", 10.8F, FontStyle.Regular, GraphicsUnit.Point, 0);
+        ficInput.Location = new Point(388, 128);
+        ficInput.Margin = new Padding(2);
+        ficInput.Name = "ficInput";
+        ficInput.Size = new Size(281, 31);
+        ficInput.TabIndex = 6;
         // 
         // label2
         // 
-        label2.Location = new System.Drawing.Point(538, 147);
+        label2.Font = new Font("Segoe UI", 7.8F);
+        label2.ForeColor = Color.WhiteSmoke;
+        label2.Location = new Point(388, 229);
+        label2.Margin = new Padding(2, 0, 2, 0);
         label2.Name = "label2";
-        label2.Size = new System.Drawing.Size(49, 20);
+        label2.Size = new Size(51, 25);
         label2.TabIndex = 7;
         label2.Text = "Página";
         // 
-        // textBox2
+        // pagina1Input
         // 
-        textBox2.Location = new System.Drawing.Point(617, 147);
-        textBox2.Name = "textBox2";
-        textBox2.Size = new System.Drawing.Size(80, 23);
-        textBox2.TabIndex = 8;
+        pagina1Input.Font = new Font("Segoe UI", 10.8F);
+        pagina1Input.Location = new Point(390, 256);
+        pagina1Input.Margin = new Padding(2);
+        pagina1Input.Name = "pagina1Input";
+        pagina1Input.Size = new Size(116, 31);
+        pagina1Input.TabIndex = 8;
         // 
-        // textBox3
+        // pagina2Input
         // 
-        textBox3.Location = new System.Drawing.Point(739, 147);
-        textBox3.Name = "textBox3";
-        textBox3.Size = new System.Drawing.Size(80, 23);
-        textBox3.TabIndex = 9;
+        pagina2Input.Font = new Font("Segoe UI", 10.8F);
+        pagina2Input.Location = new Point(544, 256);
+        pagina2Input.Margin = new Padding(2);
+        pagina2Input.Name = "pagina2Input";
+        pagina2Input.Size = new Size(125, 31);
+        pagina2Input.TabIndex = 9;
         // 
         // label3
         // 
-        label3.Location = new System.Drawing.Point(703, 150);
+        label3.Font = new Font("Segoe UI", 7.8F);
+        label3.ForeColor = Color.WhiteSmoke;
+        label3.Location = new Point(510, 265);
+        label3.Margin = new Padding(2, 0, 2, 0);
         label3.Name = "label3";
-        label3.Size = new System.Drawing.Size(30, 20);
+        label3.Size = new Size(30, 21);
         label3.TabIndex = 10;
         label3.Text = "Até";
         // 
-        // button2
+        // SaveAndPrint
         // 
-        button2.Location = new System.Drawing.Point(674, 287);
-        button2.Name = "button2";
-        button2.Size = new System.Drawing.Size(88, 44);
-        button2.TabIndex = 11;
-        button2.Text = "Executar";
-        button2.UseVisualStyleBackColor = true;
+        SaveAndPrint.Font = new Font("Segoe UI", 7.8F, FontStyle.Regular, GraphicsUnit.Point, 0);
+        SaveAndPrint.ImageAlign = ContentAlignment.BottomCenter;
+        SaveAndPrint.Location = new Point(544, 307);
+        SaveAndPrint.Margin = new Padding(2);
+        SaveAndPrint.Name = "SaveAndPrint";
+        SaveAndPrint.Size = new Size(101, 42);
+        SaveAndPrint.TabIndex = 12;
+        SaveAndPrint.Text = "Salvar e Imprimir";
+        SaveAndPrint.UseVisualStyleBackColor = true;
+        SaveAndPrint.Click += SaveAndPrint_click;
         // 
-        // label4
+        // saveButton
         // 
-        label4.Location = new System.Drawing.Point(617, 207);
-        label4.Name = "label4";
-        label4.Size = new System.Drawing.Size(49, 17);
-        label4.TabIndex = 12;
-        label4.Text = "Destino";
+        saveButton.Font = new Font("Segoe UI", 7.8F, FontStyle.Regular, GraphicsUnit.Point, 0);
+        saveButton.ImageAlign = ContentAlignment.BottomCenter;
+        saveButton.Location = new Point(401, 307);
+        saveButton.Margin = new Padding(2);
+        saveButton.Name = "saveButton";
+        saveButton.Size = new Size(105, 42);
+        saveButton.TabIndex = 13;
+        saveButton.Text = "Salvar";
+        saveButton.UseVisualStyleBackColor = true;
         // 
-        // comboBox2
+        // selectedItem
         // 
-        comboBox2.FormattingEnabled = true;
-        comboBox2.Location = new System.Drawing.Point(617, 227);
-        comboBox2.Name = "comboBox2";
-        comboBox2.Size = new System.Drawing.Size(135, 23);
-        comboBox2.TabIndex = 13;
+        selectedItem.Font = new Font("Segoe UI", 10.2F, FontStyle.Regular, GraphicsUnit.Point, 0);
+        selectedItem.ForeColor = Color.WhiteSmoke;
+        selectedItem.Location = new Point(388, 61);
+        selectedItem.Margin = new Padding(2, 0, 2, 0);
+        selectedItem.Name = "selectedItem";
+        selectedItem.Size = new Size(447, 28);
+        selectedItem.TabIndex = 14;
+        // 
+        // selectedFilePath
+        // 
+        selectedFilePath.ForeColor = Color.WhiteSmoke;
+        selectedFilePath.Location = new Point(388, 455);
+        selectedFilePath.Margin = new Padding(2, 0, 2, 0);
+        selectedFilePath.Name = "selectedFilePath";
+        selectedFilePath.Size = new Size(193, 12);
+        selectedFilePath.TabIndex = 15;
+        selectedFilePath.Visible = false;
         // 
         // HomeScreen
         // 
-        AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
-        AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-        BackColor = System.Drawing.Color.FromArgb(((int)((byte)107)), ((int)((byte)75)), ((int)((byte)151)));
-        ClientSize = new System.Drawing.Size(1226, 689);
-        Controls.Add(comboBox2);
-        Controls.Add(label4);
-        Controls.Add(button2);
+        AutoScaleMode = AutoScaleMode.None;
+        BackColor = Color.FromArgb(107, 75, 151);
+        ClientSize = new Size(1230, 705);
+        Controls.Add(selectedFilePath);
+        Controls.Add(selectedItem);
+        Controls.Add(saveButton);
+        Controls.Add(SaveAndPrint);
         Controls.Add(label3);
-        Controls.Add(textBox3);
-        Controls.Add(textBox2);
+        Controls.Add(pagina2Input);
+        Controls.Add(pagina1Input);
         Controls.Add(label2);
-        Controls.Add(textBox1);
+        Controls.Add(ficInput);
         Controls.Add(label1);
-        Controls.Add(button1);
         Controls.Add(Impressora);
-        Controls.Add(comboBox1);
+        Controls.Add(printInput);
         Controls.Add(changeFolder);
         Controls.Add(_treeView);
-        Icon = ((System.Drawing.Icon)resources.GetObject("$this.Icon"));
-        Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+        Font = new Font("Segoe UI", 2.67386913F, FontStyle.Regular, GraphicsUnit.Point, 0);
+        Margin = new Padding(3, 2, 3, 2);
+        Name = "HomeScreen";
         Text = "Footer Changer";
         Load += HomeScreen_Load;
         ResumeLayout(false);
         PerformLayout();
     }
 
-    private System.Windows.Forms.ComboBox comboBox2;
+    private System.Windows.Forms.Label selectedFilePath;
 
-    private System.Windows.Forms.Label label4;
+    private System.Windows.Forms.Label selectedItem;
 
-    private System.Windows.Forms.TextBox textBox2;
-    private System.Windows.Forms.TextBox textBox3;
+    private System.Windows.Forms.Button SaveAndPrint;
+
+    private System.Windows.Forms.TextBox pagina1Input;
+    private System.Windows.Forms.TextBox pagina2Input;
     private System.Windows.Forms.Label label3;
-    private System.Windows.Forms.Button button2;
+    private System.Windows.Forms.Button saveButton;
 
     private System.Windows.Forms.Label label1;
     private System.Windows.Forms.Label label2;
-    private System.Windows.Forms.TextBox textBox1;
-
-    private System.Windows.Forms.Button button1;
+    private System.Windows.Forms.TextBox ficInput;
 
     private System.Windows.Forms.Label Impressora;
 
-    private System.Windows.Forms.ComboBox comboBox1;
+    private System.Windows.Forms.ComboBox printInput;
 
     private System.Windows.Forms.Button changeFolder;
 
--- FooterChanger/HomeScreen.cs
+++ FooterChanger/HomeScreen.cs
@@ -1,16 +1,22 @@
+using System.Drawing.Printing;
 using FooterChanger.Helpers;
+using Spire.Doc;
+using Spire.Doc.Documents;
+using System.Drawing.Printing;
+
 
 namespace FooterChanger;
 
 public partial class HomeScreen : Form
 {
     private System.Windows.Forms.TreeView _treeView;
+
     public HomeScreen()
     {
         InitializeComponent();
         _treeView!.BeforeExpand += _treeView_BeforeExpand!;
         _treeView!.AfterSelect += _treeView_AfterSelect!;
-        
+
     }
 
     private void HomeScreen_Load(object sender, EventArgs e)
@@ -26,13 +32,15 @@ public partial class HomeScreen : Form
                 Properties.Settings.Default.Save();
                 savedPath = dialog.SelectedPath;
                 LoadDirectory(savedPath);
-                
+
             }
         }
         else
         {
             LoadDirectory(savedPath);
         }
+
+        LoadPrinters();
     }
 
     private void LoadDirectory(string path)
@@ -56,6 +64,8 @@ public partial class HomeScreen : Form
     private void _treeView_AfterSelect(object sender, TreeViewEventArgs e)
     {
         var path = e.Node?.Tag?.ToString();
+        selectedItem.Text = Path.GetFileName(path);
+        selectedFilePath.Text = path;
     }
 
     private void _treeView_BeforeExpand(object sender, TreeViewCancelEventArgs e)
@@ -75,7 +85,7 @@ public partial class HomeScreen : Form
     private void AddFiles(TreeNode node)
     {
         var path = node.Tag!.ToString();
-        
+
         var subFolders = Directory.GetDirectories(path!);
         foreach (var subFolder in subFolders)
         {
@@ -83,11 +93,11 @@ public partial class HomeScreen : Form
             {
                 Tag = subFolder
             };
-            
+
             subNode.Nodes.Add("...");
             _treeView!.Nodes.Add(subNode);
         }
-        
+
         var subFilesDoc = Directory.GetFiles(path!, "*.doc");
         foreach (var subFile in subFilesDoc)
         {
@@ -95,13 +105,13 @@ public partial class HomeScreen : Form
             {
                 Tag = subFile,
                 ForeColor = Color.WhiteSmoke,
-                
+
                 ImageIndex = 1
             };
-            
+
             node.Nodes.Add(fileNode);
         }
-        
+
         var subFilesDocx = Directory.GetFiles(path!, "*.docx");
         foreach (var subFile in subFilesDocx)
         {
@@ -111,7 +121,7 @@ public partial class HomeScreen : Form
                 ForeColor = Color.WhiteSmoke,
                 ImageIndex = 1
             };
-            
+
             node.Nodes.Add(fileNode);
         }
     }
@@ -128,18 +138,69 @@ public partial class HomeScreen : Form
         }
     }
 
-    private void label1_Click(object sender, EventArgs e)
+    private void LoadPrinters()
     {
-        throw new System.NotImplementedException();
+        printInput.Items.Clear();
+
+        foreach (string printer in PrinterSettings.InstalledPrinters)
+        {
+            printInput.Items.Add(printer);
+        }
+
+        // Define a impressora padrão como selecionada
+        var defaultPrinter = new PrinterSettings().PrinterName;
+        if (printInput.Items.Contains(defaultPrinter))
+            printInput.SelectedItem = defaultPrinter;
     }
 
-    private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
+
+    private void PrintWithFICReplacement(string filePath)
     {
-        throw new System.NotImplementedException();
+        // 1. Carrega o documento
+        Document document = new Document();
+        document.LoadFromFile(filePath);
+
+        // 2. Substitui o texto
+        string ficValue = ficInput.Text.Trim();
+        document.Replace("#FIC", ficValue, true, true);
+
+        // 3. Define a impressora selecionada
+        string selectedPrinter = printInput.SelectedItem?.ToString();
+        if (string.IsNullOrEmpty(selectedPrinter))
+        {
+            MessageBox.Show("Selecione uma impressora.");
+            return;
+        }
+
+        // 4. Configura a impressão
+        PrintDocument printDoc = document.PrintDocument;
+        printDoc.PrinterSettings.PrinterName = selectedPrinter;
+
+        // 5. Imprime
+        try
+        {
+            printDoc.Print();
+            MessageBox.Show("Documento enviado para a impressora.");
+        }
+        catch (Exception ex)
+        {
+            MessageBox.Show($"Erro ao imprimir: {ex.Message}");
+        }
     }
 
-    private void label5_Click(object sender, EventArgs e)
+    private void SaveAndPrint_click(object sender, EventArgs e)
     {
-        throw new System.NotImplementedException();
+        if (!string.IsNullOrEmpty(selectedFilePath.Text) && !(selectedFilePath.Text.Contains(".doc") || selectedFilePath.Text.Contains(".docx")))
+        {
+            MessageBox.Show("Selecione um arquivo valido");
+        }
+        
+        Document document = new Document();
+        document.LoadFromFile(selectedFilePath.Text);
+
+        // 2. Substitui o texto
+        string ficValue = ficInput.Text.Trim();
+        document.Replace("#FIC", ficValue, true, true);
+        document.SaveToFile(selectedFilePath.Text);
     }
 }
