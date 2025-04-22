3f19cef78d3ffa3d5c32839741b48c7a91c2084d
João Vitor Alencar Pontes <joao.pontes@software.com.br>
2025-04-22 14:25:44 -0300

    projeto base

---
+++ FooterChanger/HomeScreen.Designer.cs
@@ -0,0 +1,58 @@
+﻿namespace FooterChanger;
+
+partial class HomeScreen
+{
+    /// <summary>
+    ///  Required designer variable.
+    /// </summary>
+    private System.ComponentModel.IContainer components = null;
+
+    /// <summary>
+    ///  Clean up any resources being used.
+    /// </summary>
+    /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+    protected override void Dispose(bool disposing)
+    {
+        if (disposing && (components != null))
+        {
+            components.Dispose();
+        }
+
+        base.Dispose(disposing);
+    }
+
+    #region Windows Form Designer generated code
+
+    /// <summary>
+    /// Required method for Designer support - do not modify
+    /// the contents of this method with the code editor.
+    /// </summary>
+    private void InitializeComponent()
+    {
+        _treeView = new System.Windows.Forms.TreeView();
+        SuspendLayout();
+        // 
+        // _treeView
+        // 
+        _treeView.BackColor = System.Drawing.Color.FromArgb(((int)((byte)177)), ((int)((byte)161)), ((int)((byte)204)));
+        _treeView.Location = new System.Drawing.Point(14, 70);
+        _treeView.Name = "_treeView";
+        _treeView.Size = new System.Drawing.Size(264, 599);
+        _treeView.TabIndex = 0;
+        _treeView.AfterSelect += _treeView_AfterSelect;
+        // 
+        // HomeScreen
+        // 
+        AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+        AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+        BackColor = System.Drawing.Color.FromArgb(((int)((byte)107)), ((int)((byte)75)), ((int)((byte)151)));
+        ClientSize = new System.Drawing.Size(1224, 681);
+        Controls.Add(_treeView);
+        Text = "Footer Changer";
+        Load += HomeScreen_Load;
+        ResumeLayout(false);
+        PerformLayout();
+    }
+
+    #endregion
+}
---
+++ FooterChanger/HomeScreen.cs
@@ -0,0 +1,113 @@
+namespace FooterChanger;
+
+public partial class HomeScreen : Form
+{
+    private System.Windows.Forms.TreeView _treeView;
+    public HomeScreen()
+    {
+        InitializeComponent();
+        _treeView!.BeforeExpand += _treeView_BeforeExpand!;
+        _treeView!.AfterSelect += _treeView_AfterSelect!;
+    }
+
+    private void HomeScreen_Load(object sender, EventArgs e)
+    {
+        var savedPath = Properties.Settings.Default.DefaultDirectory;
+
+        if (!Directory.Exists(savedPath) && string.IsNullOrEmpty(savedPath))
+        {
+            using var dialog = new FolderBrowserDialog();
+            if (dialog.ShowDialog() == DialogResult.OK)
+            {
+                Properties.Settings.Default.DefaultDirectory = dialog.SelectedPath;
+                Properties.Settings.Default.Save();
+                savedPath = dialog.SelectedPath;
+                LoadDirectory(savedPath);
+            }
+        }
+        else
+        {
+            LoadDirectory(savedPath);
+        }
+    }
+
+    private void LoadDirectory(string path)
+    {
+        try
+        {
+            _treeView!.Nodes.Clear();
+            var root = new TreeNode(Path.GetFileName(path))
+            {
+                Tag = path,
+            };
+            root.Nodes.Add("...");
+            _treeView!.Nodes.Add(root);
+        }
+        catch (Exception ex)
+        {
+            MessageBox.Show($"Erro ao carregar pastas {ex.Message}");
+        }
+    }
+
+    private void _treeView_AfterSelect(object sender, TreeViewEventArgs e)
+    {
+        var path = e.Node?.Tag?.ToString();
+    }
+
+    private void _treeView_BeforeExpand(object sender, TreeViewCancelEventArgs e)
+    {
+        try
+        {
+            if (e.Node?.Nodes.Count != 1 || e.Node.Nodes[0].Text != "...") return;
+            e.Node.Nodes.Clear();
+            AddFiles(e.Node);
+        }
+        catch (Exception ex)
+        {
+            MessageBox.Show($"Erro ao carregar arquivos {ex.Message}");
+        }
+    }
+
+    private void AddFiles(TreeNode node)
+    {
+        var path = node.Tag!.ToString();
+        
+        var subFolders = Directory.GetDirectories(path!);
+        foreach (var subFolder in subFolders)
+        {
+            var subNode = new TreeNode(Path.GetFileName(subFolder))
+            {
+                Tag = subFolder
+            };
+            
+            subNode.Nodes.Add("...");
+            _treeView!.Nodes.Add(subNode);
+        }
+        
+        var subFilesDoc = Directory.GetFiles(path!, "*.doc");
+        foreach (var subFile in subFilesDoc)
+        {
+            var fileNode = new TreeNode(Path.GetFileName(subFile))
+            {
+                Tag = subFile,
+                ForeColor = Color.WhiteSmoke,
+                ImageIndex = 1
+            };
+            
+            node.Nodes.Add(fileNode);
+        }
+        
+        var subFilesDocx = Directory.GetFiles(path!, "*.docx");
+        foreach (var subFile in subFilesDocx)
+        {
+            var fileNode = new TreeNode(Path.GetFileName(subFile))
+            {
+                Tag = subFile,
+                ForeColor = Color.WhiteSmoke,
+                ImageIndex = 1
+            };
+            
+            node.Nodes.Add(fileNode);
+        }
+    }
+}
